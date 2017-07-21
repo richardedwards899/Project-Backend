f02fea36434e383345eddfe9afbab1ffbf671933
Richard Edwards <richardedwards899@gmail.com>
2017-07-21 13:58:32 +0100

    create schema

--- app/models/input.rb
+++ app/models/input.rb
@@ -1,2 +1,6 @@
 class Input < ActiveRecord::Base
+
+  # Input automatically has getter and setter methods for the fields in the data table.
+  # We can add our own methods here (to support the inputs_controller).
+
 end
