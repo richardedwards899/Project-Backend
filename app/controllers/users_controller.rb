2c22e357cab251476a8ab87148121b6bdf8927a1
Richard Edwards <richardedwards899@gmail.com>
2017-07-21 14:58:03 +0100

    connect controller stubs to Active Record so that real data is passed back in the response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
 
   def index
-    render :json => "You're looking for all users, huh? Well, they ain't here!"
+    users = User.all
+    # render :json => "You're looking for all users, huh? Well, they ain't here!"
+    render :json => users
   end
 
   def show
-    render :json => "You're looking for user with id "+ params[:id]+". Well, he ain't here neither. Go on, scram!"
+    user = User.find(params[:id])
+    # render :json => "You're looking for user with id "+ params[:id]+". Well, he ain't here neither. Go on, scram!"
+    render :json => user
   end
 
 end
