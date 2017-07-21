8d819e55f1aa4d56a74c8ec4e612e2e714343905
Richard Edwards <richardedwards899@gmail.com>
2017-07-21 13:51:28 +0100

    Create User and Report models

---
+++ app/models/report.rb
@@ -0,0 +1,6 @@
+class Report < ActiveRecord::Base
+
+  # Report automatically has getter and setter methods for the fields in the data table.
+  # We can add our own methods here (to support the reports_controller).
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+
+  # User automatically has getter and setter methods for the fields in the data table.
+  # We can add our own methods here (to support the users_controller).
+
+end
